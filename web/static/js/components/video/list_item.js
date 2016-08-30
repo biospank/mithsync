c7113de76db807db6ce6eb6a6233be5ec3f54f07
Ilaria Di Rosa <dirosa.ilaria@gmail.com>
2016-08-30 14:02:03 +0200

    Styling projects list

--- web/static/js/components/video/list_item.js
+++ web/static/js/components/video/list_item.js
@@ -33,20 +33,42 @@ var listItem = {
 
     return m("li", [
       m("div", { class: "projects-list__content" }, [
-        m("h5", { class: "projects-list__head" }, video.title),
-        m("p", { class: "projects-list__body" }, video.description),
-        m("small", { class: "projects-list__info" }, "16-05-2016"),
         m("div", [
-          m("a", {
-            href: "/video/" + video.id + "/edit",
-            class: "btn btn-default",
-            config: m.route 
-          }, "Edit"),
-          m("button", {
-            onclick: ctrl.delete,
-            type: "button",
-            class: "btn btn-default"
-          }, "Delete")
+          m("input", { class: "magic-radio", type: "checkbox", name: video.id, id: video.id }),
+          m("label", { for: video.id })
+        ]),
+        m(".projects-list__body", [
+          m("h5", { class: "projects-list__title" }, video.title),
+          m("p", { class: "projects-list__description" }, video.description),
+          m("label", { class: "projects-list__date" }, "16-05-2016"),
+          m("div", [
+            m("a", {
+              href: "/video/" + video.id + "/edit",
+              class: "btn btn-default",
+              config: m.route
+            }, [
+              m("i", { class: "fa fa-pencil", "aria-hidden": true })
+            ], "Edit"),
+            m("a", {
+              href: "",
+              class: "btn btn-default"
+            }, [
+              m("i", { class: "fa fa-copy", "aria-hidden": true })
+            ], "Duplicate"),
+            m("a", {
+              href: "",
+              class: "btn btn-default"
+            }, [
+              m("i", { class: "fa fa-download", "aria-hidden": true })
+            ], "Export"),
+            m("button", {
+              onclick: ctrl.delete,
+              type: "button",
+              class: "btn btn-default"
+            }, [
+              m("i", { class: "fa fa-trash", "aria-hidden": true })
+            ], "Delete")
+          ])
         ])
       ])
     ]);
