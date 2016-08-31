9edf0bfe6751abfcc75d94be6265a5d24423c252
biospank <fabio.petrucci@gmail.com>
2016-09-01 00:22:28 +0200

    attempt to include plyr.js

--- web/static/js/components/video/edit.js
+++ web/static/js/components/video/edit.js
@@ -1,6 +1,8 @@
 import mixinLayout from "../layout/mixin_layout";
 import Session from "../../models/session";
+import Video from "../../models/video";
 import Slider from "../../models/slider";
+import videoPlayback from "./video_playback";
 
 var editVideo = (function() {
 
@@ -12,26 +14,23 @@ var editVideo = (function() {
       ]),
       m(".row", [
         m(".col-sm-6", [
-          m("div", [
-            m("video",
-              {
-                id: "example_video",
-                class: "video-js vjs-default-skin",
-                controls: "",
-                preload: "metadata",
-                width: "440",
-                height: "264",
-                "data-setup": '{"controls":true}'
-              },
-            [
-              m("source",
-                {
-                  src: "https://www.youtube.com/watch?v=_WgrfEaAM4Y",
-                  type: "video/mp4"
-                }
-              )
-            ])
-          ])
+          m(videoPlayback, {
+            provider: ctrl.videoInfo().provider,
+            videoId: ctrl.videoInfo().id,
+            onReady: ctrl.initPlayer
+          })
+          // m(".video_player",
+          //   {
+          //     "data-type": "youtube", //"vimeo",
+          //     "data-video-id": "_WgrfEaAM4Y", //"180519312",
+          //     config: function(element, isInitialised, context) {
+          //       console.log(element);
+          //       console.log(isInitialised);
+          //       console.log(context);
+          //       plyr.setup('.video_player');
+          //     }
+          //   }
+          // )
         ]),
         m(".col-sm-6", [
           m("div", [
@@ -52,6 +51,11 @@ var editVideo = (function() {
     controller: function() {
       var ctrl = this;
 
+      ctrl.video = m.prop({});
+      ctrl.videoInfo = m.prop({});
+      ctrl.errors = m.prop({});
+      ctrl.player = {};
+
       if(Session.isExpired()) {
         m.route("/signin");
       }
@@ -59,6 +63,67 @@ var editVideo = (function() {
       ctrl.initializeSlider = function() {
         Slider.init("slider");
       };
+
+      // the first argument is the DOM element;
+      // the second argument is false if the element has just been created and true otherwise;
+      // the third argument is the context – it allows you to define
+      // extra behaviour before the element is removed from the DOM.
+      // Probably the best way to think about this in a jQuery way
+      // is that config is a bit like DOM ready
+      ctrl.initPlayer = function(element, init, context) {
+        if( !init ) {
+          // m.startComputation();
+          // ctrl.player = plyr.setup('.video_player');
+          ctrl.player = plyr.setup('.video_player', {
+            //['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'fullscreen']
+            controls: ['play', 'progress', 'current-time', 'mute', 'volume']
+          })[0];
+
+          ctrl.player.on('ready', function(event) {
+            console.log('ready!');
+            var duration = moment("2015-01-01").startOf('day')
+              .seconds(ctrl.player.getDuration())
+              .format('HH:mm:ss');
+            console.log(duration);
+          });
+
+          ctrl.player.on('error', function(error) {
+            console.log(error);
+          });
+
+          ctrl.player.on('seeked', function(event) {
+            console.log(event);
+            console.log("seeked");
+          });
+
+          ctrl.player.on('stalled', function(event) {
+            console.log(event);
+            console.log("stalled");
+          });
+
+          ctrl.player.on('timeupdate', function(event) {
+            var duration = moment("2015-01-01").startOf('day')
+              .seconds(ctrl.player.getCurrentTime())
+              .format('HH:mm:ss');
+            console.log(duration);
+          });
+          //console.log(ctrl.player);
+          // m.endComputation();
+        }
+      };
+
+      ctrl.getVideo = function(videoId) {
+        return Video.show(videoId).then(function(video) {
+          ctrl.video(video.data);
+          ctrl.videoInfo(Video.info(ctrl.video().url));
+        }, function(response) {
+          ctrl.errors(response.errors);
+        })
+      };
+
+      Video.init();
+
+      ctrl.getVideo(m.route.param("videoId"));
 
     },
     view: mixinLayout(content)
--- web/static/js/models/video.js
+++ web/static/js/models/video.js
@@ -3,6 +3,7 @@ import Session from './session'
 
 var Video = {
   url: '/videos',
+  urlParser: {},
   model: {
     title: m.prop(""),
     description: m.prop(""),
@@ -33,6 +34,17 @@ var Video = {
       }, args)
     );
   },
+  show: function(id) {
+    return m.request({
+      method: "GET",
+      url: Videosync.apiBaseUrl() +
+        this.url + "/" + id,
+      config: function(xhr) {
+        xhr.setRequestHeader("accept", "application/json");
+        xhr.setRequestHeader("Authorization", Videosync.realm + " " + Session.token())
+      }
+    });
+  },
   delete: function(id) {
     return m.request(
       {
@@ -45,6 +57,27 @@ var Video = {
         }
       }
     );
+  },
+  init: function() {
+    this.urlParser = new UrlParser();
+    this.urlParser.bind(new Vimeo());
+    this.urlParser.bind(new YouTube());
+  },
+  info: function(url) {
+    return urlParser.parse(url);
+    //   /*
+    //   * Tested URLs:
+    // ko   var url = 'http://youtube.googleapis.com/v/4e_kz79tjb8?version=3';
+    // ok  url = 'https://www.youtube.com/watch?feature=g-vrec&v=Y1xs_xPb46M';
+    // ok  url = 'http://www.youtube.com/watch?feature=player_embedded&v=Ab25nviakcw#';
+    // ok  url = 'http://youtu.be/Ab25nviakcw';
+    // ok  url = 'http://www.youtube.com/watch?v=Ab25nviakcw';
+    // ok  url = '<iframe width="420" height="315" src="http://www.youtube.com/embed/Ab25nviakcw" frameborder="0" allowfullscreen></iframe>';
+    // ko  url = '<object width="420" height="315"><param name="movie" value="http://www.youtube-nocookie.com/v/Ab25nviakcw?version=3&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube-nocookie.com/v/Ab25nviakcw?version=3&amp;hl=en_US" type="application/x-shockwave-flash" width="420" height="315" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
+    // ok  url = 'http://i1.ytimg.com/vi/Ab25nviakcw/default.jpg';
+    // ok  url = 'https://www.youtube.com/watch?v=BGL22PTIOAM&feature=g-all-xit';
+    // ko  url = 'BGL22PTIOAM';
+    //   */
   }
 };
 
