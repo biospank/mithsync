7dc358e2b6a640db5cf970a2860f551c2a483227
Ilaria Di Rosa <dirosa.ilaria@gmail.com>
2016-09-10 14:00:55 +0200

    Test thumbnail

--- web/static/js/components/video/slick_carousel.js
+++ web/static/js/components/video/slick_carousel.js
@@ -25,18 +25,20 @@ var slickCarousel = (function() {
     view: function(ctrl, args, slides) {
       carousel.slides = slides;
 
-      return m("div", { class: "slidesheet", config: ctrl.onReady }, [
-        carousel.slides.map(function(slide) {
-          return m(slickItem, {
-            selectCallback: function(slide) {
-              carousel.currentSlide = slide;
+      return m("section", { class: "slidesheet", config: ctrl.onReady }, [
+        m(".row", [
+          carousel.slides.map(function(slide) {
+            return m(slickItem, {
+              selectCallback: function(slide) {
+                carousel.currentSlide = slide;
 
-              if(args.selectCallback)
-                args.selectCallback(slide);
+                if(args.selectCallback)
+                  args.selectCallback(slide);
 
-            }
-          }, slide, _.isEqual(slide, carousel.currentSlide));
-        })
+              }
+            }, slide, _.isEqual(slide, carousel.currentSlide));
+          })
+        ])
       ]);
     }
   }
