4fb229378112935c6d7241e0a07910ee37371695
biospank <fabio.petrucci@gmail.com>
2016-09-20 14:04:10 +0200

    Integrate reveal library Make it works with video preview

--- web/static/js/components/video/preview.js
+++ web/static/js/components/video/preview.js
@@ -6,6 +6,24 @@ import videoPlayback from './video_playback';
 var preview = {
   controller: function(video) {
     var videoInfo = Video.info(video.url);
+    var currentSlide = undefined;
+
+    var updateSlide = function(event) {
+      var currentSec = _.floor(event.detail.plyr.getCurrentTime());
+
+      if(currentSlide)
+        if(_.inRange(currentSec, currentSlide.start, (currentSlide.end + 1)))
+          return;
+
+      currentSlide = _.find(video.slides, function(slide, index, collection) {
+        if(_.inRange(currentSec, slide.start, (slide.end + 1))) {
+          Reveal.slide(index);
+          return true;
+        }
+
+        return false;
+      });
+    }
 
     return {
       onReady: function(element, isInit, context) {
@@ -18,6 +36,45 @@ var preview = {
           player.on('ready', function(event) {
             console.log('player ready!');
           });
+
+          player.on('timeupdate', _.throttle(updateSlide, 1000, {
+            'leading': true,
+            'trailing': false
+          }));
+
+          // player.on('timeupdate', _.debounce(function(event) {
+          //   console.log(player.getCurrentTime());
+          // }, 1000, {
+          //   'leading': true,
+          //   'trailing': false,
+          //   'maxWait': 1000
+          // }));
+
+          Reveal.initialize({
+            // Flags if the presentation is running in an embedded mode,
+            // i.e. contained within a limited portion of the screen
+            embedded: true,
+            // Display controls in the bottom right corner
+            controls: false,
+            // Display a presentation progress bar
+            progress: false,
+            // Enable keyboard shortcuts for navigation
+            keyboard: false,
+            // Enable the slide overview mode
+            overview: false,
+            // Vertical centering of slides
+            center: false,
+            // Enables touch navigation on devices with touch input
+            touch: false,
+            // Enables touch navigation on devices with touch input
+            help: false,
+            // Transition style for full page slide backgrounds
+            backgroundTransition: 'none',
+            // Number of slides away from the current that are visible
+            viewDistance: 1,
+            width: 'auto',
+            height: 'auto'
+          });
         }
       },
       videoInfo: function() {
@@ -25,7 +82,7 @@ var preview = {
       }
     };
   },
-  view: function(ctrl) {
+  view: function(ctrl, video) {
     return m(".row", [
       m(".col-sm-6", [
         m(videoPlayback, {
@@ -36,11 +93,16 @@ var preview = {
         })
       ]),
       m(".col-sm-6", [
-        m("figure", { class: "placeholder" }, [
-          m("img", {
-            src: "/images/contentplaceholder.png",
-            class: "img-responsive"
-          })
+        m(".reveal", {}, [
+          m(".slides", [
+            video.slides.map(function(slide) {
+              return m("section", [
+                m("img", {
+                  src: slide.url
+                })
+              ])
+            })
+          ])
         ])
       ])
     ]);
