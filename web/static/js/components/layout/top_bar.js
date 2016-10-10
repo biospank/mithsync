964b1f6807080097e19effe3b899560399277efc
biospank <fabio.petrucci@gmail.com>
2016-10-10 17:52:52 +0100

    fix

--- web/static/js/components/layout/top_bar.js
+++ web/static/js/components/layout/top_bar.js
@@ -1,6 +1,27 @@
 import user from "./user";
 
 var topBar = {
+  showMenuButton: function() {
+    if(/videos\/\d+\/edit/i.exec(m.route())) {
+      return m("a", {
+        href: "#",
+        class: "navbar-button",
+        id: "menu-toggle",
+        config: function(element, isInit, context) {
+          if(!isInit) {
+            $(element).click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+          }
+        }
+      }, [
+        m("i", { class: "fa fa-bars", "aria-hidden": true })
+      ])
+    } else {
+      return "";
+    }
+  },
   view: function(ctrl) {
     return m("nav", { class: "topbar navbar-fixed-top" }, [
       m(".container", [
@@ -9,9 +30,7 @@ var topBar = {
             m("a", { href: "#", class: "navbar-brand" }, [
               m("img", { src: "/images/logo.jpg", alt: "Zinkroo" })
             ]),
-            m("button", { class: "navbar-button" }, [
-              m("i", { class: "fa fa-bars", "aria-hidden": true })
-            ])
+            this.showMenuButton()
           ]),
           m(".pull-right", [
             m.component(user)
