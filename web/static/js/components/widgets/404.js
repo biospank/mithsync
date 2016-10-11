523dada84f21c85c0be88f2462169bd4fc6d6eb2
biospank <fabio.petrucci@gmail.com>
2016-10-11 12:12:57 +0100

    fix

--- web/static/js/components/widgets/404.js
+++ web/static/js/components/widgets/404.js
@@ -2,7 +2,7 @@ var recordNotFound = {
   view: function() {
     return m("", [
       m("figure", { class: "text-center" }, [
-        m("img", { src: "images/no-list.svg", with: "100", height: "100" })
+        m("img", { src: "images/no-list.svg", with: "80", height: "80" })
       ]),
       m("h3", { class: "text-center mgv20" }, "No records found"),
       m("p", { class: "text-center" }, "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.")
