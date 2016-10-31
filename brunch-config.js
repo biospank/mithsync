b8d8757a8755c4728e4d1e2cbfdb28ed589d76cd
biospank <fabio.petrucci@gmail.com>
2016-10-31 17:33:23 +0100

    extract watch script into js file

--- brunch-config.js
+++ brunch-config.js
@@ -5,7 +5,7 @@ exports.config = {
       // joinTo: "js/app.js",
       joinTo: {
        "js/app.js": /^(web\/static\/js)|(web\/static\/vendor)|(deps)/,
-       "js/watch.js": /^(web\/static\/watch\/vendor)|(deps)|(priv\/static\/phoenix*)/
+       "js/watch.js": /^(web\/static\/watch)|(deps)|(priv\/static\/phoenix*)/
       },
 
       // To use a separate vendor.js bundle, specify two files path
@@ -60,13 +60,14 @@ exports.config = {
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor|web\/static\/watch/]
+      ignore: [/web\/static\/vendor|web\/static\/watch\/vendor/]
     }
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["web/static/js/app"]
+      "js/app.js": ["web/static/js/app"],
+      "js/watch.js": ["web/static/watch/app/js/watch"]
     }
   },
 
---
+++ web/static/watch/app/js/watch.js
@@ -0,0 +1,109 @@
+// import videoSlides from "./video_slides";
+
+document.addEventListener('DOMContentLoaded', function() {
+  var slides = [];
+  var currentSlide = undefined;
+  var timeVector = [];
+
+  var inRange = function(progress, idx1, idx2) {
+    return _.inRange(progress, timeVector[idx1], (timeVector[idx2] || 100000));
+  };
+
+  var updateSlide = function(event) {
+    var currentSec = _.floor(event.detail.plyr.getCurrentTime());
+
+    if(currentSlide) {
+      var currentSlideIndex = _.findIndex(slides, currentSlide);
+
+      if(inRange(currentSec, currentSlideIndex, (currentSlideIndex + 1))) {
+        return;
+      }
+    }
+
+    currentSlide = _.find(slides, function(slide, index, collection) {
+      if(inRange(currentSec, index, (index + 1))) {
+        Reveal.slide(index);
+        return true;
+      }
+
+      return false;
+    });
+  };
+
+  document.body.querySelectorAll('img').forEach(function(img) {
+    slides.push({
+      src: img.src,
+      start: img.dataset.start
+    });
+    timeVector.push(img.dataset.start);
+  });
+
+  var player = plyr.setup('.video-player-watch', {
+    //['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'fullscreen']
+    controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'current-time']
+  })[0];
+
+  player.on('ready', function(event) {
+    console.log('player ready!');
+  });
+
+  player.on('timeupdate', _.throttle(updateSlide, 1000, {
+    'leading': true,
+    'trailing': false
+  }));
+
+  Reveal.initialize({
+    // Flags if the presentation is running in an embedded mode,
+    // i.e. contained within a limited portion of the screen
+    embedded: true,
+    // Display controls in the bottom right corner
+    controls: false,
+    // Display a presentation progress bar
+    progress: false,
+    // Enable keyboard shortcuts for navigation
+    keyboard: false,
+    // Enable the slide overview mode
+    overview: false,
+    // Vertical centering of slides
+    center: false,
+    // Enables touch navigation on devices with touch input
+    touch: false,
+    // Enables touch navigation on devices with touch input
+    help: false,
+    // Transition style
+    transition: 'slide', // none/fade/slide/convex/concave/zoom
+    // Transition speed
+    transitionSpeed: 'slow', // default/fast/slow
+    // Transition style for full page slide backgrounds
+    // backgroundTransition: 'none',
+    // Number of slides away from the current that are visible
+    viewDistance: 1,
+    width: 'auto',
+    height: 'auto'
+  });
+});
+
+// $(document).ready(function() {
+//   console.log('retrieving #video-player-watch tag');
+//   var $playerTag = $('#video-player-watch');
+//
+//   if($playerTag) {
+//     var urlParser = new UrlParser();
+//
+//     // urlParser.addProvider('vimeo');
+//     urlParser.addProvider('youtube');
+//     var videoInfo = urlParser.parse(url);
+//
+//     $playerTag.data('type', videoInfo.provider);
+//     $playerTag.data('video-id', videoInfo.videoId);
+//
+//     var player = plyr.setup('#video-player-watch', {
+//       //['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'fullscreen']
+//       controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'current-time']
+//     })[0];
+//
+//     player.on('ready', function(event) {
+//       console.log('player ready!');
+//     });
+//   }
+// })
