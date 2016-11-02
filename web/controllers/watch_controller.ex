6aa3f023913a0bf13924dfa68a1edd3eb79d013d
biospank <fabio.petrucci@gmail.com>
2016-11-02 11:05:32 +0100

    add plug to remove x-frame-header on watch controller

--- web/controllers/watch_controller.ex
+++ web/controllers/watch_controller.ex
@@ -5,18 +5,17 @@ defmodule Videosync.WatchController do
   alias Videosync.Slide
 
   plug :put_layout, "watch.html"
+  plug :remove_x_frame_options_header
 
   def show(conn, %{"id" => id}) do
     video = Video
       |> Video.preload_slides(Slide.order_by(:start))
       |> Repo.get!(id)
 
-    conn
-      |> remove_x_frame_options_header
-      |> render("show.html", video: video)
+    render(conn, "show.html", video: video)
   end
 
-  defp remove_x_frame_options_header(conn) do
+  defp remove_x_frame_options_header(conn, _opts) do
     delete_resp_header(conn, "x-frame-options")
   end
 end
