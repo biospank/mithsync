2418a7d00c09f117d75d662485c8929b4d0821b3
biospank <fabio.petrucci@gmail.com>
2016-12-07 11:09:17 +0100

    Use Map get instead of fetch to get whatever is into filter options

--- test/lib/image_proxy_test.exs
+++ test/lib/image_proxy_test.exs
@@ -38,8 +38,8 @@ defmodule Videosync.ImageProxyTest do
   end
 
   @tag :local
-  test "list local scoped files without filter", %{scope: scope} do
-    {:ok, list} = ImageProxy.list(%{ scope: scope, filter: nil })
+  test "list local scoped files", %{scope: scope} do
+    {:ok, list} = ImageProxy.list(%{ scope: scope })
     refute Enum.empty? list
   end
 
@@ -56,8 +56,8 @@ defmodule Videosync.ImageProxyTest do
   end
 
   @tag :s3
-  test "list s3 scoped files without filter", %{scope: scope} do
-    {:ok, list} = ImageProxy.list(%{ scope: scope, filter: nil })
+  test "list s3 scoped files", %{scope: scope} do
+    {:ok, list} = ImageProxy.list(%{ scope: scope })
     refute Enum.empty? list
   end
 
