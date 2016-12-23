35901e6f7eb0bd3b05558f7589db93b2afb976ac
biospank <fabio.petrucci@gmail.com>
2016-12-23 16:07:20 +0100

    Add style on menu top

--- web/static/js/components/layout/top_bar.js
+++ web/static/js/components/layout/top_bar.js
@@ -22,19 +22,35 @@ var topBar = {
       return "";
     }
   },
+  showMenuTop: function() {
+    if(/videos\/\d+\/edit/i.exec(m.route()) == null) {
+      return m("nav", { class: "top-nav", id: "" }, [
+        m("ul", { class: "nav nav-pills" }, [
+          m("li", { role: "presentation", class: "active" }, [
+            m("a", { href: "#", class: "text-uppercase" }, "Create new project")
+          ]),
+          m("li", { role: "presentation" }, [
+            m("a", { href: "#", class: "text-uppercase" }, "My project")
+          ])
+        ])
+      ])
+    }
+  },
   view: function(ctrl) {
     return m("nav", { class: "topbar navbar-fixed-top" }, [
       m(".clearfix", {}, [
         m(".pull-left", [
-          // m("a", { href: "#", class: "navbar-brand" }, [
-          //   m("img", { src: "/images/logo.jpg", alt: "Zinkroo" })
-          // ]),
           m("a", {
             href: "/dashboard",
             config: m.route,
-            class: "navbar-brand"
-          }, "Zinkroo"),
+            class: "navbar-brand pull-left"
+          }, [
+            m("img", { src: "images/logo-zinkroo-azzurro.png", class: "img-responsive" })
+          ]),
           this.showMenuButton()
+        ]),
+        m(".pull-left", [
+          this.showMenuTop()
         ]),
         m(".pull-right", [
           m.component(user)
