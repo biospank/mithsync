0c698228a129877b6d4821925880cd6a39b3a37f
biospank <fabio.petrucci@gmail.com>
2017-02-09 13:07:32 +0100

    Fix setting active class on menu's buttons

--- web/static/js/components/layout/main_nav.js
+++ web/static/js/components/layout/main_nav.js
@@ -36,8 +36,8 @@ var mainNav = {
   view: function(ctrl) {
     return m("nav", { class: "main-nav"}, [
       m("ul", { class: "nav nav-pills nav-stacked" }, [
-        m("li", { class: (ctrl.isActive("/") ? 'active main-nav__tab' : 'main-nav__tab') }, [
-          m("a", { href: "/", config: m.route, class: "" }, [
+        m("li", { class: (ctrl.isActive("/dashboard") ? 'active main-nav__tab' : 'main-nav__tab') }, [
+          m("a", { href: "/dashboard", config: m.route, class: "" }, [
             m("i", { class: "fa fa-dashboard main-nav__icon" }),
             m("span", { class: "main-nav__voice" }, "Dashboard")
           ])
