8fe28feca054f6c471f84c86a5c5a63cedb3301d
Ilaria Di Rosa <dirosa.ilaria@gmail.com>
2017-02-20 12:16:32 +0100

    Remove google plus social from social sharing box

--- web/static/js/components/widgets/social_share.js
+++ web/static/js/components/widgets/social_share.js
@@ -14,12 +14,6 @@ var socialShare = {
         ])
       ]),
       m("li", { class: "social-share__item" }, [
-        m("a", { href: "https://plus.google.com/share?url=http://www.zinkroo.com", class: "googleplus", target: "_blank" }, [
-          m("i", { class: "fa fa-google-plus", "aria-hidden": "true" })
-          // m("label", "Share it")
-        ])
-      ]),
-      m("li", { class: "social-share__item" }, [
         m("a", { href: "http://www.linkedin.com/shareArticle?mini=true&url=http://www.zinkroo.com/?/&title=Zinkroo&summary=The%20perfect%20way%20to%20sync%20and%20embed%20your%20video%20and%20images%2C%20everywhere!", class: "linkedin", target: "_blank" }, [
           m("i", { class: "fa fa-linkedin", "aria-hidden": "true" })
           // m("label", "Share it")
