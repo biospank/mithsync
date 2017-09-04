df1e24ed30ae05e1adc357da93b3ff5214a00860
biospank <fabio.petrucci@gmail.com>
2017-09-04 15:34:31 +0200

    fix image dialog

--- web/static/js/components/video/image_dialog.js
+++ web/static/js/components/video/image_dialog.js
@@ -7,127 +7,134 @@ import imageItem from "./image_item";
 import Project from "../../models/project";
 import Video from "../../models/video";
 
-const imageDialog = {
-  oninit(vnode) {
-    this.images = m.stream(undefined);
-    this.errors = m.stream({});
-    this.filter = m.stream("");
-    this.pageInfo = {};
+const imageDialog = (() => {
+  let images = m.stream(undefined);
+  let errors = m.stream({});
+  let filter = m.stream("");
+  let pageInfo = {};
+  let unwrapSuccess = (response) => {
+    if(response) {
+      this.pageInfo = {
+        totalEntries: response.total_entries,
+        totalPages: response.total_pages,
+        pageNumber: response.page_number
+      };
 
-    this.unwrapSuccess = (response) => {
-      if(response) {
-        this.pageInfo = {
-          totalEntries: response.total_entries,
-          totalPages: response.total_pages,
-          pageNumber: response.page_number
-        };
+      return response;
+    }
+  }
 
-        return response;
-      }
-    };
+  let getImages = (params) => {
+    images(undefined);
 
-    this.getImages = (params) => {
-      this.images(undefined);
-      // console.log(Project.current());
-      // console.log(Video.current());
-      return Image.all(
-          {
-            projectId: Project.current().id,
-            videoId: Video.current().id
-          },
-          params,
-          { background: true }
-      ).then(this.unwrapSuccess).then((response) => {
-        this.images(response.data);
-        m.redraw();
-      }, (response) => {
-        // in case of 404 http status code
-        // response is undefined: (cannot extract json data)
-        this.images([]);
-        m.redraw();
-      })
-    };
+    return Image.all(
+      {
+        projectId: Project.current().id,
+        videoId: Video.current().id
+      },
+      _.assign(params, { page_size: 18 }),
+      { background: true }
+    ).then(unwrapSuccess).then((response) => {
+      images(response.data);
+      m.redraw();
+    }, (response) => {
+      // in case of 404 http status code
+      // response is undefined: (cannot extract json data)
+      images([]);
+      m.redraw();
+    })
+  };
 
-    this.filterImages = (event) => {
-      event.preventDefault();
+  let filterImages = (event) => {
+    event.preventDefault();
 
-      this.getImages(
-        _.assign(
-          this.pageInfo.defaultParams || {},
-          { page: 1 }
-        )
-      );
-    };
-
-    this.imageView = () => {
-      if(!this.images()) {
-        return m(loader);
-      } else {
-        return _.isEmpty(this.images()) ? m(recordNotFound) : this.images().map((image) => {
-          return m(imageItem, {key: image.id, image: image, callback: () => {}});
-        });
-      }
-    };
-
-  },
-  show(opts) {
-    $("#imageDialog").fadeIn( "fast" );
-    // $("#imageDialog").modal('show');
     getImages(
       _.assign(
-        {},
+        pageInfo.defaultParams || {},
         { page: 1 }
       )
     );
-  },
-  view({state}) {
-    return m(".modal image-modal-lg", {
-      "tabindex": "-1",
-      "role": "dialog",
-      "id": "imageDialog"
-    }, [
-      m(".modal-dialog modal-lg", {
-        role: "document"
-        // class: "bounceIn animated"
+  };
+
+  let selectCallback = () => {};
+
+  let imageView = () => {
+    if(!images()) {
+      return m(loader);
+    } else {
+      return _.isEmpty(images()) ? m(recordNotFound) : images().map((image) => {
+        return m(imageItem, {key: image.id, image: image, callback: selectCallback});
+      });
+    }
+  };
+
+  return {
+    show(opts) {
+      selectCallback = (image) => {
+        if(opts['selectCallback'] !== undefined)
+          opts['selectCallback'](image);
+
+        $("#imageDialog").hide();
+      }
+
+      $("#imageDialog").fadeIn( "fast" );
+      // $("#imageDialog").modal('show');
+      getImages(
+        _.assign(
+          pageInfo.defaultParams || {},
+          { page: 1 }
+        )
+      );
+    },
+    view(vnode) {
+      return m(".modal image-modal-lg", {
+        "tabindex": "-1",
+        "role": "dialog",
+        "id": "imageDialog"
       }, [
-        m(".modal-content", [
-          m(".modal-header", [
-            m("button.close", {
-              onclick: () => {
-                $("#imageDialog").hide();
-              }
-            }, [
-              m("span", { "aria-hidden": "true" }, m.trust("&times"))
-            ]),
-            m(searchForm, {
-              action: state.filterImages,
-              filter: state.filter
-            })
-          ]),
-          m(".modal-body", [
-            m("section", { class: "slidesheet slidesheet-height" }, [
-              m("div", { class: "row" }, [
-                state.imageView()
-              ])
-            ]),
-            m(new Pagination(),
-              _.assign(
-                state.pageInfo,
-                {
-                  xhr: (params) => {
-                    state.getImages(params);
-                  },
-                  defaultParams: {
-                    filter: state.filter()
-                  }
+        m(".modal-dialog modal-lg", {
+          role: "document"
+          // class: "bounceIn animated"
+        }, [
+          m(".modal-content", [
+            m(".modal-header", [
+              m("button.close", {
+                onclick: () => {
+                  $("#imageDialog").hide();
                 }
+              }, [
+                m("span", { "aria-hidden": "true" }, m.trust("&times"))
+              ]),
+              m(searchForm, {
+                action: filterImages,
+                filter: filter
+              })
+            ]),
+            m(".modal-body", [
+              m("section", { class: "slidesheet slidesheet-height" }, [
+                m("div", { class: "row" }, [
+                  imageView()
+                ])
+              ]),
+              m(new Pagination(),
+                _.assign(
+                  pageInfo,
+                  {
+                    xhr: (params) => {
+                      getImages(params);
+                    },
+                    defaultParams: {
+                      filter: filter()
+                    }
+                  }
+                )
               )
-            )
+            ])
           ])
         ])
       ])
-    ])
+    }
   }
-};
+})();
 
 export default imageDialog;
